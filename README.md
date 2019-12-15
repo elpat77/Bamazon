6721cc057e98a9bb0fc0b7087ff3e33bd06abed4
elpat7 <aguilarpat7@gmail.com>
2019-12-15 14:15:27 -0800

    updating readme and adding functions to update db

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Bamazon
+# Welcome to Bamazon!
+
+* This is an Amazon-like storefront using MySQL and node.js creating during class. The app will take in orders from customers and deplete stock from the store's inventory.
+
+
+**What Each JavaScript Does**
+_bamazonCustomer.js_
+
+*Prints the products in the store.
+
+*Prompts customer which product they would like to purchase by ID number.
+
+*Asks for the quantity.
+
+*If there is a sufficient amount of the product in stock, it will return the total for that purchase.
+However, if there is not enough of the product in stock, it will tell the user that there isn't enough of the product.
+If the purchase goes through, it updates the stock quantity to reflect the purchase.
+It will also update the product sales in the department table.
+
+
+**Running the application**
+
+*Clone repo.
+*Run command in Terminal or Gitbash 'npm install'
+*Run command depending which mode you would like to be on:
+*Customer - 'node bamazonCutomer.js'
+
+Run 'ctrl + c' to exit each mode
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var inquirer = require('inquirer');
 var itemSelected;
 var purchaseAmount;
 var salesAmount;
-var newStock;
+var newStock = [];
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -14,14 +14,14 @@ const connection = mysql.createConnection({
     insecurerAuth: true
 })
 
-// console.log(connection);
-
 connection.connect(err => {
     if (err) throw err;
     // console.log(`you are connected to thread ID: ${connection.threadId}`);
-    selectProduct()
+    selectProduct();
     showTable('products');
-    connection.end();
+    // updateTable(23, 'Mens Barcelona Jersey Large');
+    // addTo('products', { item_id: "SFWGH-01", product_name: "SF Warriors Girls Hat", department_name: "Sports", sub_department: "Basketball", price: "15.99", stock_quantity: '12' })
+
 })
 
 function selectProduct() {
@@ -29,13 +29,10 @@ function selectProduct() {
         connection.query(`SELECT * from ??`, table, (err, res) => {
             if (err) throw (err);
             console.table(res);
-            console.log(res.length);
             var products = [];
             for (let i = 0; i < res.length; i++) {
-                // console.log(products);
                 products.push(res[i].product_name);
             }
-            // console.log(products.length)
             inquirer.prompt([{
                 type: 'list',
                 name: "choice",
@@ -49,30 +46,22 @@ function selectProduct() {
                 name: 'amount',
             }
             ]).then(function (selection) {
-                // var itemSelected;
-                // var purchaseAmount;
-                // var salesAmount;
-
                 //check what ids the product name
                 for (let i = 0; i < res.length; i++) {
                     if (res[i].product_name === selection.choice) {
                         itemSelected = res[i];
                         // find how much the person ordered with is input
                         purchaseAmount = selection.amount;
-
                     }
                 }
                 //compare that amount with stock quantity
                 //if you have enough, then sell it and substract that item
                 if (purchaseAmount <= itemSelected.stock_quantity) {
-
                     salesAmount = itemSelected.price * purchaseAmount;
-                    console.log("The item you selected is in stock");
+                    console.log(itemSelected.product_name, "is in sock");
                     console.log("The price of the item is: $", itemSelected.price);
                     console.log("Your total purchase amount will be: $", parseFloat(salesAmount));
                     confirmPurchase();
-
-
                 }
                 //if don't have enough then "error message or shop for something else"
                 else {
@@ -93,6 +82,17 @@ function confirmPurchase() {
         if (purchase.selected === 'Yes please!') {
             console.log("Thanks for your purchase, your order will ship out soon!");
             newStock = itemSelected.stock_quantity - purchaseAmount;
+            productBought = itemSelected.product_name;
+            // console.log(itemSelected.id);
+            // console.log("new stock is", newStock);
+            // console.log("product name", productBought);
+            // updateInventory(newStock, productBought);
+            connection.query('UPDATE products SET ? WHERE ?', [{ stock_quantity: newStock }, { item_id: itemSelected.id }],
+                (err) => {
+                    if (err) throw (err);
+                });
+
+            process.exit();
 
         } else {
             console.log('Thanks for visiting, come back soon!');
@@ -101,7 +101,33 @@ function confirmPurchase() {
     })
 }
 
-// function updateInventory() {
-
-
+// addTo = (table, obj) => {
+//     connection.query("INSERT INTO ?? SET ?", [table, obj],
+//         (err, res) => {
+//             if (err) throw err;
+//         });
 // }
+
+
+// updateTable = (updateQuanitity, product_name) => {
+//     connection.query('UPDATE products SET ? Where product_name = ?',
+//         [
+//             { stock_quantity: updateQuanitity },
+//             { product_name }],
+//         (err, res) => {
+//             if (err) throw (err);
+//             console.table(res);
+//         })
+// }
+
+updateInventory = (quantity, product_name) => {
+    connection.query(
+        `UPDATE bamazon.products SET stock_quantity = ${quantity} WHERE product_name = ${product_name}`,
+        err => {
+            if (err) console.log(err);
+            console.log(
+                chalk.green("You have successfully updated the inventory")
+            );
+        }
+    );
+};
